4ebcbccd08a10aca68cc717dce909f1548b8e98c
Saul Chavez Desktop <romsc93@live.com.mx>
2015-10-18 23:45:10 -0500

    Minor fix in hlsl

--- Shaders/ALU_Copy.hlsl
+++ Shaders/ALU_Copy.hlsl
@@ -5,9 +5,6 @@ RWTexture2D<float4>	Output:register(u0);	// UAV
 [numthreads(8, 8, 1)]
 void Main(uint3 id:SV_DispatchThreadID)
 {
-	[numthreads(8, 8, 1)]
-void Main(uint3 id:SV_DispatchThreadID)
-{
 	Output[id.xy] = Input[id.xy];
 }
 
